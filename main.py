6717ec1d3fb73e31fc059b2b36f6bb75920dfce6
patrickvanamen <patrick@vanamen.net>
2025-08-05 21:56:26 +0200

    Constants, FPS etc

--- main.py
+++ main.py
@@ -7,12 +7,17 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while "x" == "x":
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
         pygame.display.flip()
+
+        #limit the FPS to 60
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
